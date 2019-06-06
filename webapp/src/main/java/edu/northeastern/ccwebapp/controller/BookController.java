6eb1cd7bc12660a19994432a7d4c3ebdba096822
Tanmayee Kalluri <kalluri.t@husky.neu.edu>
2019-06-06 11:35:42 -0700

    Test case for getting a book by ID

--- webapp/src/main/java/edu/northeastern/ccwebapp/controller/BookController.java
+++ webapp/src/main/java/edu/northeastern/ccwebapp/controller/BookController.java
@@ -1,7 +1,6 @@
 package edu.northeastern.ccwebapp.controller;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
