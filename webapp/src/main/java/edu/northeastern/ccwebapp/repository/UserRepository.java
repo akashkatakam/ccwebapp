d0689578476dc5b310f493d1da052399caa7d471
Tanmayee Kalluri <44352243+tankalluri@users.noreply.github.com>
2019-06-07 19:11:04 -0700

    Revert "Assignment1"

--- webapp/src/main/java/edu/northeastern/ccwebapp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.northeastern.ccwebapp.repository;
-
-import edu.northeastern.ccwebapp.pojo.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User , Long> {
-        User findByUsername(String username);
-}
