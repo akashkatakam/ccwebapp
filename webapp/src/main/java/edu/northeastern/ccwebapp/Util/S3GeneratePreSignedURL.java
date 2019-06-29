0e98d3b8702424d3d95798737e59268b0633ea78
Akash Katakam <katakam.a@husky.neu.edu>
2019-06-28 20:16:09 -0400

    Fixed aws credential issue in AWS

--- webapp/src/main/java/edu/northeastern/ccwebapp/Util/S3GeneratePreSignedURL.java
+++ webapp/src/main/java/edu/northeastern/ccwebapp/Util/S3GeneratePreSignedURL.java
@@ -2,28 +2,24 @@ package edu.northeastern.ccwebapp.Util;
 
 import com.amazonaws.HttpMethod;
 import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import edu.northeastern.ccwebapp.service.S3Config;
 
 import java.net.URL;
 
 public class S3GeneratePreSignedURL {
 
     public String getPreSignedURL(String objKey, String domainName) {
-        String clientRegion = "us-east-1";
         URL url=null;
-
-        try {            
-            AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(clientRegion) .withCredentials(new ProfileCredentialsProvider()) .build();
+        try {
+            AmazonS3 s3Client = S3Config.amazonS3Client();
     
             // Set the presigned URL to expire after 2min.
             java.util.Date expiration = new java.util.Date();
             long expTimeMillis = expiration.getTime();
             expTimeMillis += 1000 * 60 * 2;
             expiration.setTime(expTimeMillis);
-
             // Generate the presigned URL.
             System.out.println("Generating pre-signed URL.");
             GeneratePresignedUrlRequest generatePresignedUrlRequest =  new GeneratePresignedUrlRequest(domainName, objKey).withMethod(HttpMethod.GET) .withExpiration(expiration);
