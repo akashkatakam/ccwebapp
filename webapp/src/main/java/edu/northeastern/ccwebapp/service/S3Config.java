1f2fd75d56243bde73d610eb80f5b1640e139b58
Akash Katakam <katakam.a@husky.neu.edu>
2019-07-12 16:20:00 -0400

    PresignedURL Issue trail and error

--- webapp/src/main/java/edu/northeastern/ccwebapp/service/S3Config.java
+++ webapp/src/main/java/edu/northeastern/ccwebapp/service/S3Config.java
@@ -1,7 +1,6 @@
 package edu.northeastern.ccwebapp.service;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.auth.SystemPropertiesCredentialsProvider;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +12,7 @@ public class S3Config {
     @Bean
     public static AmazonS3Client amazonS3Client() {
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withCredentials(new SystemPropertiesCredentialsProvider())
+                .withCredentials(new InstanceProfileCredentialsProvider())
                 .build();
     }
 }
