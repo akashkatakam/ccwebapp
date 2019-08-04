cc41deaf7dfa445e327e2ba4dda4f13c06c92019
Tanmayee Kalluri <kalluri.t@husky.neu.edu>
2019-08-04 13:13:46 -0700

    Auto scaling application stack JSON, create and delete script

---
+++ infrastructure/aws/cloudformation/application/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#Script to create the application stack
+
+
+display_usage()
+{
+echo "Usage:$0 [StackName] [KeyPairName] [networkStackName] [policyStackName]"
+}
+if [[ $# -lt 4 ]];then
+	display_usage
+	exit 1
+fi
+
+appStackName=$1
+echo "App stack name:"$appStackName
+keyPairName=$2
+networkStackName=$3
+policyStackName=$4
+
+amiId=$(aws ec2 describe-images --owners self --query 'reverse(sort_by(Images,&CreationDate)[].ImageId)[0]' --output text)
+echo "Selected AMI Id-> "$amiId
+domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+name=${domain::-1}
+BucketName="${name}.csye6225.com"
+echo ${BucketName}
+StackID=$(aws cloudformation create-stack --stack-name ${appStackName} --template-body file://csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=KeyPair,ParameterValue=${keyPairName} ParameterKey=ImageID,ParameterValue=${amiId} ParameterKey=S3Bucket,ParameterValue=${BucketName} ParameterKey=NetworkStackName,ParameterValue=${networkStackName} ParameterKey=PolicyStackName,ParameterValue=${policyStackName}|grep StackId)
+if [[ $? -eq 0 ]]; then
+echo "Stack Creation initiated"
+    stackCompletion=$(aws cloudformation wait stack-create-complete --stack-name ${appStackName})
+        if [[ $? -eq 0 ]]; then
+            echo "Stack with name $appStackName creation completed successfully!!"
+            exit 0
+        else
+            echo "Error creating CloudFormation Application Stack"
+        fi
+else
+    echo "Error creating CloudFormation Application Stack"
+    echo ${StackID}
+    exit 1
+fi
